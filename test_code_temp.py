e41cbc417ee241b1da13fd6c553a6c4fb15785be
lugai <634594053@qq.com>
2023-11-27 15:46:32 +0800

    change test_code.py

---
+++ test_code_temp.py
@@ -0,0 +1,25 @@
+import numpy as nb
+import yaml
+# with open("baseline.yaml", "r") as fout:
+#     params = yaml.load(fout, Loader = yaml.FullLoader)
+# print(params)
+# print(params["--export_dir"])
+apiData = {
+   "page": 1,
+   "msg": "地址",
+   "data": [{
+      "id": 1,
+      "name": "学校"
+   }, {
+      "id": 2,
+      "name": "公寓"
+   }, {
+      "id": 3,
+      "name": "流动人口社区"
+   }],
+}
+with open("baseline.yaml", "w", encoding='utf-8') as fout:
+    yaml.dump(data = apiData, stream = fout, allow_unicode = True)
+    yaml.load(fout, Loader = yaml.FullLoader)
+
+   
